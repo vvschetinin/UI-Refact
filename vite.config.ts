6c32bc74e898e42b8eeed8ea03931304c4296035
Vladlen Schetinin <vvschetinin@gmail.com>
2025-05-21 18:41:35 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import path from "path";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+
+const aliases = {
+  "@": fileURLToPath(new URL("./src", import.meta.url)),
+  "@scss": fileURLToPath(new URL("./src/assets/scss", import.meta.url)),
+};
+
+const root = fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig(({ mode }) => ({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(root, "index.html"),
+        services: path.resolve(root, "pages/services/index.html"),
+        cases: path.resolve(root, "pages/cases/index.html"),
+        about: path.resolve(root, "pages/about/index.html"),
+        blog: path.resolve(root, "pages/blog/index.html"),
+        faq: path.resolve(root, "pages/faq/index.html"),
+        contacts: path.resolve(root, "pages/contacts/index.html"),
+        // ======================== inner services ========================
+        aibots: path.resolve(root, "pages/services/aibots/index.html"),
+        aicontent: path.resolve(root, "pages/services/aicontent/index.html"),
+        aihelpers: path.resolve(root, "pages/services/aihelpers/index.html"),
+        aiseo: path.resolve(root, "pages/services/aiseo/index.html"),
+        // ================================================================
+        err404: path.resolve(root, "pages/error/index.html"),
+      },
+    },
+  },
+  resolve: {
+    alias: aliases,
+  },
+  plugins: [],
+  css: {
+    postcss: "./postcss.config.js",
+    devSourcemap: mode === "development",
+  },
+}));
