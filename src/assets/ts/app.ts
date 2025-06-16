478b455dfb461787494313427ffd8c11cc74a14e
Vladlen Schetinin <vvschetinin@gmail.com>
2025-06-16 18:27:32 +0300

    Поправки отображения видео

--- src/assets/ts/app.ts
+++ src/assets/ts/app.ts
@@ -1,4 +1,4 @@
-import "@/assets/ts/components/inputmask/inputmask";
+// import "@/assets/ts/components/inputmask/inputmask";
 import "@/assets/ts/components/myslider/main";
 import "@/assets/ts/components/formsend/formsend";
 import "@/assets/ts/main/script";
